6428153bc5d035f2cbe30b826d55e303c19e2f57
Your Name <nords3x4@gmail.com>
2017-12-18 18:57:04 -0800

    cubednumbers solved and added readme files

---
+++ arraychanger/arraychanger.js
@@ -0,0 +1,16 @@
+'use strict'
+
+var words = ['this', 'that', 'other']
+
+function formatWords(words){
+  for (var i = 0; i < words.length; i++) {
+  if (i != words[words.length - 1]) {
+    words.join(',')
+    console.log(words)
+  } else {
+    words[words]
+  }
+    return words;
+}}}
+
+formatWords(words)
